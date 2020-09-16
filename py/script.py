63b3546fa357e09a53789ed13bbc8859aeec27ca
maarten <maarten.van.elst@gmail.com>
2020-09-16 14:31:27 +0200

    isolate tags from data

--- py/script.py
+++ py/script.py
@@ -1,2 +1,9 @@
-print('Hello, Space!')
+#!/usr/bin/env python3
+### The line above here tells the computer to execute this file with python3, so in the command line you can just do 'script.py' to run this script.
+sourcefile = '../data/Space_Corrected.csv'
 
+with open(sourcefile, 'r') as filehandle:
+	tags = filehandle.readline().strip().split(',')
+
+import pprint as pp
+pp.pprint(tags)
