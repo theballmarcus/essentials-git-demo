5536fa9f04748de0fe009edf5fd763cf9ba1665b
maarten <maarten.van.elst@gmail.com>
2020-09-16 15:55:44 +0200

    preprocess data

---
+++ py/preprocess_data.py
@@ -0,0 +1,22 @@
+### Preprocessing ###
+
+# We create another data source, with the mission name in a separate column from the rocket name
+# Additionally, remove the first column from all lines to prevent confusion
+
+sourcefile = '../data/Space_Corrected.csv'
+outputfile = '../data/Space_Processed.csv'
+
+headers = ''
+fileout = ''
+
+with open(sourcefile, 'r') as filehandle:
+	line = filehandle.readline().replace(' | ',',').strip().split(',')
+	headers += ','.join(line[1:5] + ['Project_Name'] + line[6:]) + '\n'
+	while len(line) > 1:
+		line = filehandle.readline().replace(' | ',',').strip().split(',')[1:]
+		fileout += ','.join(line[1:]) + '\n'
+
+with open(outputfile, 'w') as filehandle:
+	filehandle.write(headers)
+	filehandle.write(fileout)
+	
--- py/script.py
+++
@@ -1,8 +0,0 @@
-sourcefile = '../data/Space_Corrected.csv'
-
-with open(sourcefile, 'r') as filehandle:
-	tags = filehandle.readline().strip().split(',')
-
-import pprint as pp
-pp.pprint(tags)
-
